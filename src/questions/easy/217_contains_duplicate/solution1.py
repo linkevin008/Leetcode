a5e954120c2f94160462819b1e6990c5df605fa6
Kevin Lin <linkevin008@gmail.com>
2025-05-03 23:03:03 -0700

    add 217

---
+++ src/questions/easy/217_contains_duplicate/solution1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        '''
+        use a counter and return True if a counter value contains > 2
+        else return False
+        '''
+
+        counter = Counter(nums)
+
+        for count in counter.values():
+            if count >= 2:
+                return True
+
+        return False

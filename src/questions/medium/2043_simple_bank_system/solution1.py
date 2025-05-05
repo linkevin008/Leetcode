56441fa6ec55d883685f7701fb1d5032896b5100
Kevin Lin <linkevin008@gmail.com>
2025-05-04 19:39:18 -0700

    add 2043

---
+++ src/questions/medium/2043_simple_bank_system/solution1.py
@@ -0,0 +1,40 @@
+class Bank:
+
+    def __init__(self, balance: List[int]):
+        self.accounts = dict()
+
+        for i, individual_balance in enumerate(balance):
+            self.accounts[i + 1] = individual_balance
+        
+
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        if account1 in self.accounts and account2 in self.accounts and self.accounts[account1] >= money:
+            self.accounts[account1] -= money
+            self.accounts[account2] += money
+            return True
+        else:
+            return False
+
+    def deposit(self, account: int, money: int) -> bool:
+        if account in self.accounts:
+            self.accounts[account] += money
+            return True
+        else:
+            return False
+        
+
+    def withdraw(self, account: int, money: int) -> bool:
+        if account in self.accounts and self.accounts[account] >= money:
+            self.accounts[account] -= money
+            return True
+        else:
+            return False
+
+        
+
+
+# Your Bank object will be instantiated and called as such:
+# obj = Bank(balance)
+# param_1 = obj.transfer(account1,account2,money)
+# param_2 = obj.deposit(account,money)
+# param_3 = obj.withdraw(account,money)
